9a6aa140c8cd5a6c55524189956c30feae579f24
T3tje <tilman2013@gmail.com>
2023-11-20 11:09:33 +0100

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+
+
+        // Specify the future date using a LocalDate object
+        LocalDate futureDate = LocalDate.of(2023, 8, 5);
+
+        // Combine the future date with a default time (midnight) to create a LocalDateTime object
+        LocalDateTime futureDateTime = futureDate.atStartOfDay();
+
+        // Compare the dates and times
+        if (localDateTime.isBefore(futureDateTime)) {
+            System.out.println("The current date and time is before the specified future date and time.");
+        } else if (localDateTime.isAfter(futureDateTime)) {
+            System.out.println("The current date and time is after the specified future date and time.");
+        } else {
+            System.out.println("The current date and time is the same as the specified future date and time.");
+        }
+
+        LocalDateTime ldt1 = LocalDateTime.of(2021, 1, 1, 0, 0 ,0, 0);
+        LocalDateTime ldt2 = LocalDateTime.of(2023, 1, 1, 0, 0 ,0, 0);
+        long days = ChronoUnit.DAYS.between(ldt1, ldt2);
+        long months = ChronoUnit.MONTHS.between(ldt1, ldt2);
+        System.out.println("Days between: " + days);
+        System.out.println("Months betweeen: " +months);
+    }
+}
